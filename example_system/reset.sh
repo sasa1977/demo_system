19344fac4883ddcc839e9f398a866a5995fc1b42
Sasa Juric <sasa.juric@gmail.com>
2019-04-13 18:28:55 +0200

    add a reset script

---
+++ example_system/reset.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eo pipefail
+
+_build/prod/rel/system/bin/system stop > /dev/null 2>&1 || true
+rm -rf _build/prod/rel/system > /dev/null 2>&1 || true
+git reset HEAD --hard
+MIX_ENV=dev mix compile
+MIX_ENV=test mix compile
+mix release
+_build/prod/rel/system/bin/system start
