feb6759adb46171abe8a1a9007f1c020e60fcc79
Seungjin Kim <skim@proove.com>
2021-09-05 21:44:25 -0700

    UPDATE: fix some tests, but the large input test should be failing because the sum() is a dumb, intentionally long-time taking one

--- example_system/test/math_test.exs
+++ example_system/test/math_test.exs
@@ -3,17 +3,20 @@ defmodule ExampleSystem.MathTest do
   use ExUnitProperties
   import StreamData
   alias ExampleSystem.Math
+  @error_raising_input_value 13
 
   property "correct sum is returned for valid input" do
-    check all n <- valid_input() do
-      assert {:ok, pid} = Math.sum(n)
-      assert_receive({:sum, ^pid, sum})
-      assert sum == Enum.sum(1..n)
+    check all(n <- valid_input()) do
+      if n != @error_raising_input_value do
+        assert {:ok, pid} = Math.sum(n)
+        assert_receive({:sum, ^pid, sum})
+        assert sum == Enum.sum(1..n)
+      end
     end
   end
 
   property "the caller receives a down message when the sum process finishes" do
-    check all n <- valid_input() do
+    check all(n <- valid_input()) do
       ExUnit.CaptureLog.capture_log(fn ->
         assert {:ok, pid} = Math.sum(n)
         assert_receive {:DOWN, _mref, :process, ^pid, _reason}
